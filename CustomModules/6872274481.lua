7a4076b226c803f71dfa06a9e0291f83985b69ed
NeptuneRbx <keshauhbancroft@gmail.com>
2024-10-05 21:12:59 +0100

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -5073,6 +5073,32 @@ run(function()
 		Function = function(callback) if FPSBoost.Enabled then FPSBoost.ToggleButton(false) FPSBoost.ToggleButton(false) end end
 	})
 end)
+																																																																																																								
+run(function()
+    local Disabler = {Enabled = false}
+    Disabler = GuiLibrary.ObjectsThatCanBeSaved.WorldWindow.Api.CreateOptionsButton({
+        Name = "InfiniteJump", 
+        Function = function(callback)
+            if callback then
+                local UserInputService = game:GetService("UserInputService")
+                local player = game.Players.LocalPlayer
+                local character = player.Character or player.CharacterAdded:Wait()
+                local humanoid = character:WaitForChild("Humanoid")
+
+                UserInputService.JumpRequest:Connect(function()
+                    if Disabler.Enabled then
+                        humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                    end
+                end)
+
+                Disabler.Enabled = true
+            else
+                Disabler.Enabled = false
+            end
+        end,
+        HoverText = "Allows Humanoid To Jump Infinitely"
+    })
+end)																																																																																																								
 
 run(function()
 	local GameFixer = {Enabled = false}
